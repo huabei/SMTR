9826eb1a9cf9b6683299b023fca1d596bffe5a51
王华北 <951946366@qq.com>
2022-04-25 14:14:14 +0800

    complete code

--- miRNA_Structure/extract_ares_top_pdbs.py
+++ miRNA_Structure/extract_ares_top_pdbs.py
@@ -8,7 +8,7 @@ import numpy as np
 pyfile_name, input_file, output_folder, ares_scores_file, top_n = argv
 
 # root_dir = pathlib.Path(__file__).parent.absolute()
-root_dir = '..'
+root_dir = '.'
 input_path = os.path.join(root_dir, input_file)
 output_path = os.path.join(root_dir, output_folder)
 ares_scores_path = os.path.join(root_dir, ares_scores_file)
@@ -17,12 +17,25 @@ ares_scores_path = os.path.join(root_dir, ares_scores_file)
 scores = np.loadtxt(ares_scores_path)
 sort_arg_scores = np.argsort(scores)
 tags = list()
-for tag_n in sort_arg_scores[:3]:
+top = 0
+mv_command = ''
+for tag_n in sort_arg_scores[:int(top_n)]:
+    top += 1
     tag_n += 1000001
+    
     tag = 'S_' + str(tag_n)[1:]
+    new_tag = 'Top_' + str(top) + '_S_' + str(tag_n)[1:] + '.pdb'
     tags.append(tag)
+    mv_command += 'mv '
+    mv_command += tag + '.pdb '
+    mv_command += os.path.join(output_path, new_tag)
+    mv_command += ';'
+    # new_tags.append(new_tag)
     
 EXE = 'extract_pdbs'
-command = '%s -in:file:silent %s -in:file:tags %s' % (EXE, outfilename, " ".join(tags))
-system(command)
+extract_command = '{0} -in:file:silent {1} -in:file:tags {2}'.format(EXE, input_path, " ".join(tags))
+system(extract_command)
+
+# mv_command = 'mv {0} {1}'.format(' '.join(tags), output_path)
+system(mv_command)
 
