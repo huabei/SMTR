31bf3cc0cee52888b3968c073c482fdd2ab37b60
王华北 <951946366@qq.com>
2022-05-21 12:03:18 +0800

    dev

--- smtr/smtr_e3nn_debug.py
+++ smtr/smtr_e3nn_debug.py
@@ -7,7 +7,7 @@ import time
 from smtr_e3nn_model import SMTR, create_transform, Ligand_dataset
 
 # %%
-learning_rate = 1e-5
+learning_rate = 1e-3
 max_epoch = 5
 if_new_train = True
 check_point = '20220516_114821.ckpt'
@@ -27,8 +27,8 @@ transform = create_transform()
 # data
 train_dataset = Ligand_dataset(train_dataset, transform=transform)
 val_dataset = Ligand_dataset(val_dataset, transform=transform)
-train_dataloader = torch_geometric.loader.DataLoader(train_dataset, batch_size=8)
-val_dataloader = torch_geometric.loader.DataLoader(val_dataset, batch_size=8)
+train_dataloader = torch_geometric.loader.DataLoader(train_dataset, batch_size=1)
+val_dataloader = torch_geometric.loader.DataLoader(val_dataset, batch_size=1)
 
 
 
@@ -39,8 +39,10 @@ if if_new_train:
     smnn = SMTR(learning_rate=learning_rate)
 else:
     smnn = SMTR().load_from_checkpoint(checkpoint_path=check_point)
-trainer = pl.Trainer(logger=wandb_logger, max_epochs=max_epoch, accumulate_grad_batches=2, terminate_on_nan=True,
-                        auto_scale_batch_size=True, accelerator='gpu', devices=1)
+# trainer = pl.Trainer(logger=wandb_logger, max_epochs=max_epoch, accumulate_grad_batches=2, terminate_on_nan=True,track_grad_norm=2,
+#                     accelerator='gpu', devices=1)
+trainer = pl.Trainer(logger=wandb_logger, max_epochs=max_epoch, terminate_on_nan=True,
+                    accelerator='gpu', devices=1)
 
 
 
